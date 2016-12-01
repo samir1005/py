cf2e8f4d51054ab9f5876169bf37e416231295a9
samir1005 <idarapro@gmail.com>
2016-12-01 16:58:38 +0100

    Update time-stat.py

--- time-stat.py
+++ time-stat.py
@@ -5,7 +5,7 @@ import time
 #now= datetime.datetime.now().hour
 now = time.strftime("%H:%M")
 if now == '09:00' or now =='11:51' :
-    #exec (open("./path/to/script.py").read(), globals())
-    os.system('C:\Users\silent\Desktop\python-3.5.2.exe')
+  
+    os.system('File ')
 else:
     print 'not yet'
